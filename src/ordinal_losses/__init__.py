1e0beed6f81be47a009e2a142f34da90d613d51d
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2022-08-17 12:24:04 +0100

    Bug fix: CDW_CE not working when using GPU due to wrong device.

--- src/ordinal_losses/__init__.py
+++ src/ordinal_losses/__init__.py
@@ -171,7 +171,7 @@ class CDW_CE(CrossEntropy):
 
     def __call__(self, Yhat, Y):
         Yhat = F.softmax(Yhat, 1)
-        return -torch.mean(torch.log(1-Yhat) * torch.abs(torch.arange(self.K)[None]-Y[:, None])**self.alpha)
+        return -torch.mean(torch.log(1-Yhat) * torch.abs(torch.arange(self.K, device=Y.device)[None]-Y[:, None])**self.alpha)
 
 # Our losses that promote unimodality.
 # Notice that our losses require extra parameters: lamda and omega.
